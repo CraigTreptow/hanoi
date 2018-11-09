e9711f561049a91cc960f689d2d40185aa899486
Craig Treptow <craig.treptow@gmail.com>
2018-11-08 19:18:54 -0600

    A little nicer tower printing

--- hanoi/lib/hanoi/tower.ex
+++ hanoi/lib/hanoi/tower.ex
@@ -1,13 +1,24 @@
 defmodule Hanoi.Tower do
 
-  def display([head | [] ]), do: print_disk(head)
+  def display([head | [] ]), do: print_disk(head, 0)
   def display([head | tail]) do
-    print_disk(head)
+    max = Enum.max(tail)
+    print_disk(head, max)
     display(tail)
   end
 
-  defp print_disk(disk) do
-    String.duplicate("_", disk)
+  defp print_disk(disk, 0) do
+    lines = String.duplicate("_", disk)
+
+    lines
+    |> IO.puts
+  end
+
+  defp print_disk(disk, max) do
+    spaces = String.duplicate(" ", round((max/2) - (disk/2)))
+    lines = String.duplicate("_", disk)
+
+    spaces <> lines
     |> IO.puts
   end
 end
